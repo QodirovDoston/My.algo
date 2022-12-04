709f30a56849af0bb3f401d66026b8de044e49a9
QodirovDoston <qodirovdoston806@gmail.com>
2022-12-04 08:56:17 +0300

    doston

---
+++ script.js
@@ -0,0 +1,50 @@
+// function add(name,age,job){
+//     return (`mening ismim ${name}. yoshim ${age}, men ${job} ishlayman`)
+
+// }
+// console.log(add("doston",32,"developer"));
+
+/////////////
+// function getInfo(a,b,c){
+//     let data ={
+//         a:a,
+//         b:b,
+//         c:c
+//     }
+//     return `mening isnim ${data.a}. yoshim ${data.b}. men ${data.c} ishlayman`
+// }
+// console.log(getInfo("doston",22,"devoloper"));
+
+//////////////////////////////////
+
+// function addd(as){
+//     return as+1
+// }
+// console.log(addd(-6));
+
+//////////////////////////
+
+// function convert(sd){
+//     return sd*60
+// }
+// console.log(convert(5));
+
+/////////////
+
+// function triareal(d,s){
+//     return  d*s/2
+     
+// }
+// console.log(triareal(3,2));
+
+// function addition(num,sum){
+// return num + sum
+// }
+// console.log(addition(7,3));
+
+////////////////
+
+// function as(a,s){
+//     return a+s
+// }
+// console.log(as(10,10));
